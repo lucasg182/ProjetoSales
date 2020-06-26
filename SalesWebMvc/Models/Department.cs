5a321cd6874bd0cbdb2f56918b1b7ca52a479b7b
lucasg182 <lucasgsantos3@gmail.com>
2020-06-26 01:56:05 -0300

    Primeiro Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
