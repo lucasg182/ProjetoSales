23784068fe12a4896c0cd4a2e4fd1ab7c8b1a736
lucasg182 <lucasgsantos3@gmail.com>
2020-06-27 02:37:42 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -10,5 +10,26 @@ namespace SalesWebMvc.Models
     {
         public string Name { get; set; }
         public int Id { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
