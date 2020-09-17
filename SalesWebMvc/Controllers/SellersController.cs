3dacfb14a8d744ebd09afa8ef112d242d48f4304
lucasg182 <lucasgsantos3@gmail.com>
2020-09-17 00:24:58 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -22,5 +22,17 @@ namespace SalesWebMvc.Controllers
 
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellersService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
